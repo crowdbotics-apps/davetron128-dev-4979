be480e4ebae6a1c017aa5e72b09db4033c486b3e
Crowdbotics <team@crowdbotics.com>
2020-05-25 21:32:38 +0000

    Update models

--- davetron128_dev_4979/wsgi.py
+++ davetron128_dev_4979/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'davetron128_dev_4979.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "davetron128_dev_4979.settings")
 
 application = get_wsgi_application()
